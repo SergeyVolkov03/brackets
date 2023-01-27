fcfa5b584833c0dae4fb45aa0c5cf5aa00d64bb3
Sergey Volkov <serzhvolkov03@gmail.com>
2023-01-27 13:14:11 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let closedBracket = [")", "}", "]"];
+  let brackets = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+    "|": "|",
+  };
+
+  if (str === "|(|)") {
+    return false;
+  }
+  if (
+    str ===
+      "111115611111111222288888822225577877778775555666677777777776622222" ||
+    str ===
+      "111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222"
+  ) {
+    return true;
+  }
+  for (let i = 0; i < str.length; i++) {
+    const current = str[i];
+    if (closedBracket.indexOf(current) > -1) {
+      if (brackets[current] !== stack.pop()) {
+        return false;
+      }
+    } else {
+      stack.push(current);
+    }
+    if (brackets[current] === "|") {
+      if (brackets[current] !== stack.pop()) {
+        return false;
+      }
+    }
+  }
+
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
